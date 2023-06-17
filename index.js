9da7d6020a8b565df993957bfc3374a3debe5c67
unknown <kadersarikaya0123@gmail.com>
2023-06-17 19:50:27 +0300

    upload imgs

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
@@ -22,10 +24,33 @@ mongoose
     console.error(err);
   });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 // Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename:(req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({storage});
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  try {
+    return res.status(200).json('File uploded successfully');
+  }
+  catch (error) {
+    console.error(error);
+  }
+})
+
 
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute);
