11c89668623e3c888a4c11d34634ceb1906ad9ae
unknown <kadersarikaya0123@gmail.com>
2023-06-18 12:19:39 +0300

    a

--- routes/posts.js
+++ routes/posts.js
@@ -97,23 +97,6 @@ router.get("/profile/:id", async (req, res) => {
   }
 });
 
-// get timeline posts
-router.get("/timeline/:userId", async (req, res) => {
-  try {
-    const currentUser = await User.findById(req.params.userId);
-    const userPosts = await Post.find({ userId: currentUser._id });
-    const friendPosts = await Promise.all(
-      currentUser.followings.map((friendId) => {
-        return Post.find({ userId: friendId });
-      })
-    );
-    res.json(userPosts.concat(...friendPosts));
-  }
-  catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // get all posts
 router.get("/getAllPosts", async (req, res) => {
   try {
