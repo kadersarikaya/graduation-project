02ccac4054c405ed8a0255123e76f35fe3d7855a
unknown <kadersarikaya0123@gmail.com>
2023-06-18 12:02:29 +0300

    cors

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const multer = require("multer");
 const path = require("path");
+const cors = require('cors');
 
 dotenv.config();
 
@@ -30,6 +31,7 @@ app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+app.use(cors());
 
 
 const storage = multer.diskStorage({
