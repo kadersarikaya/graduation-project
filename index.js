6c06d1f29fcdece1687d21639a8c4e4b305d534e
unknown <kadersarikaya0123@gmail.com>
2023-06-21 14:52:48 +0300

    cors allow

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(cors());
 
+// Enable CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/images");
